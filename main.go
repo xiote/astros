262767f87191a288172cd41ca2ddab64e2051ae8
xiote <xiote@nate.com>
2020-01-29 03:15:18 +0900

    Add main func

--- main.go
+++ main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"fmt"
+)
+
+type astrosDataFormat struct {
+	People  []peopleDataFormat `json:"people"`
+	Number  int                `json:"number"`
+	Message string             `json:"message"`
+}
+type peopleDataFormat struct {
+	Name  string `json:"name"`
+	Craft string `json:"craft"`
+}
+
+func main() {
+
+	url := "http://api.open-notify.org/astros.json"
+	json := Get(url)
+
+	astros := astrosDataFormat{}
+	Decode(string(json), &astros)
+
+	fmt.Printf("\n\n json object:::: %+v", astros)
+}
